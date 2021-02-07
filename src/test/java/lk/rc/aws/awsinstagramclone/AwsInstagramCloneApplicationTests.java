4523e40722ba8f8e330ecac65124f70a6b56b278
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2021-02-07 20:40:16 +0530

    Initial Commit

---
+++ src/test/java/lk/rc/aws/awsinstagramclone/AwsInstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.rc.aws.awsinstagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsInstagramCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
